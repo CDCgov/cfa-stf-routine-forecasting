e851a5ffb01292ff25ec20008f1afb7c9d750cbf
Damon Bayer <xum8@cdc.gov>
2026-04-21 11:15:22 -0500

    add location list

---
+++ cfa/stf/forecasttools/__init__.py
@@ -0,0 +1,4 @@
+"""Forecasttools helpers exposed through the cfa.stf.forecasttools namespace."""
+
+from .ft_lite import *
+from .ft_lite import __all__
--- cfa/stf/forecasttools/ft_lite.py
+++ cfa/stf/forecasttools/ft_lite.py
@@ -3,6 +3,9 @@ import polars as pl
 from pipelines.utils.common_utils import run_r_code
 
 
+__all__ = ["get_us_loc_pop_tbl", "LOCATION_LIST"]
+
+
 def get_us_loc_pop_tbl():
     r_code = """
     dplyr::left_join(
@@ -14,3 +17,66 @@ def get_us_loc_pop_tbl():
     """
     tbl_csv = run_r_code(r_code).stdout
     return pl.read_csv(tbl_csv)
+
+
+# get_us_loc_pop_tbl().get_column("abbr").to_list()
+LOCATION_LIST = [
+    "US",
+    "AL",
+    "AK",
+    "AZ",
+    "AR",
+    "CA",
+    "CO",
+    "CT",
+    "DE",
+    "DC",
+    "FL",
+    "GA",
+    "HI",
+    "ID",
+    "IL",
+    "IN",
+    "IA",
+    "KS",
+    "KY",
+    "LA",
+    "ME",
+    "MD",
+    "MA",
+    "MI",
+    "MN",
+    "MS",
+    "MO",
+    "MT",
+    "NE",
+    "NV",
+    "NH",
+    "NJ",
+    "NM",
+    "NY",
+    "NC",
+    "ND",
+    "OH",
+    "OK",
+    "OR",
+    "PA",
+    "RI",
+    "SC",
+    "SD",
+    "TN",
+    "TX",
+    "UT",
+    "VT",
+    "VA",
+    "WA",
+    "WV",
+    "WI",
+    "WY",
+    "AS",
+    "GU",
+    "MP",
+    "PR",
+    "UM",
+    "VI",
+]

ebded4e9f08025adc33c1b56cbe713b7da8439c2
Damon Bayer <xum8@cdc.gov>
2026-04-21 11:00:11 -0500

    add loc_pop_tbl

---
+++ cfa/stf/forecasttools/ft_lite.py
@@ -0,0 +1,16 @@
+import polars as pl
+
+from pipelines.utils.common_utils import run_r_code
+
+
+def get_us_loc_pop_tbl():
+    r_code = """
+    dplyr::left_join(
+    forecasttools::us_location_table,
+    forecasttools::us_location_pop,
+    by = "name"
+) |>
+    readr::write_csv(file = stdout(), na = "")
+    """
+    tbl_csv = run_r_code(r_code).stdout
+    return pl.read_csv(tbl_csv)

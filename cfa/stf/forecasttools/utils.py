a64f2b51db4d6410033d7ff0cd5106c2ac0b47c6
Damon Bayer <xum8@cdc.gov>
2026-05-06 11:10:41 -0500

    more types allowed

--- cfa/stf/forecasttools/utils.py
+++ cfa/stf/forecasttools/utils.py
@@ -1,31 +1,14 @@
-from collections.abc import MutableSequence
+from collections.abc import Iterable
 
 import polars as pl
 import polars.selectors as cs
 
 
-def ensure_listlike(x):
-    """
-    Ensure that an object either behaves like a
-    :class:`MutableSequence` and if not return a
-    one-item :class:`list` containing the object.
-    Useful for handling list-of-strings inputs
-    alongside single strings.
-    Based on this _`StackOverflow approach
-    <https://stackoverflow.com/a/66485952>`.
-
-    Parameters
-    ----------
-    x
-        The item to ensure is :class:`list`-like.
-    Returns
-    -------
-    MutableSequence
-        ``x`` if ``x`` is a :class:`MutableSequence`
-        otherwise ``[x]`` (i.e. a one-item list containing
-        ``x``.
-    """
-    return x if isinstance(x, MutableSequence) else [x]
+def ensure_listlike[T](x: T | Iterable[T]) -> list[T]:
+    if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
+        return list(x)
+    else:
+        return [x]
 
 
 def coalesce_common_columns(

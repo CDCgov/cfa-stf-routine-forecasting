b05aa303d0ae749d410e5f13f089322379812cdc
Damon Bayer <xum8@cdc.gov>
2026-08-07 16:02:05 -0500

    Drop data_type from model inputs

--- stfroutineforecasting/tests/testthat/test_timeseries_utils.R
+++ stfroutineforecasting/tests/testthat/test_timeseries_utils.R
@@ -11,6 +11,40 @@ common_required_columns <- c(
 
 base_date <- as.Date("2024-01-01")
 
+test_that("load_training_data returns training rows without data_type", {
+  withr::with_tempdir({
+    model_dir <- fs::path(
+      "covid-19_r_2024-01-03_f_2024-01-01_t_2024-01-02",
+      "model_runs",
+      "CA",
+      "fable_e_daily"
+    )
+    fs::dir_create(fs::path(model_dir, "data"))
+    input_data <- tibble::tibble(
+      date = rep(base_date + 0:2, each = 2),
+      geo_value = "CA",
+      disease = "COVID-19",
+      data_type = rep(c("train", "train", "eval"), each = 2),
+      .variable = rep(c("observed_ed_visits", "other_ed_visits"), 3),
+      .value = c(10, 90, 11, 99, 12, 108),
+      lab_site_index = NA_integer_,
+      resolution = "daily"
+    )
+    readr::write_tsv(
+      input_data,
+      fs::path(model_dir, "data", "combined_data.tsv")
+    )
+
+    result <- load_training_data(model_dir)
+
+    expect_equal(result$data$date, base_date + 0:1)
+    expect_false("data_type" %in% colnames(result$data))
+    expect_equal(result$geo_value, "CA")
+    expect_equal(result$disease, "COVID-19")
+    expect_equal(result$resolution, "daily")
+  })
+})
+
 test_that("format_timeseries_output formats forecast data correctly", {
   # Create minimal test data
   forecast_data <- tibble::tibble(

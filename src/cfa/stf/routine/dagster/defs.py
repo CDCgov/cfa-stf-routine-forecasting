20ca2252abdcc92d67445e1aa2892013c3f7f243
jkislin <qxk3@cdc.gov>
2026-09-17 22:42:29 +0000

    remove unused commented code

--- src/cfa/stf/routine/dagster/defs.py
+++ src/cfa/stf/routine/dagster/defs.py
@@ -70,29 +70,3 @@ defs = dg.Definitions(
         ],
     ),
 )
-
-# # Create Definitions object
-# defs = dg.load_definitions_from_modules(
-#     modules=[assets, automation, jobs],
-#     resources={
-#         # These IOManagers let Dagster serialize asset outputs and store them
-#         # in Azure to pass between assets
-#         "io_manager": ADLS2PickleIOManager(),
-#         # Shared resources for model assets
-#         "model_base_config": asset_config.ModelBaseConfig(),
-#         "pyrenew_config": asset_config.PyrenewConfig(),
-#         "epiautogp_e_pct_epiweekly_config": asset_config.EpiAutoGPEPctEpiweeklyConfig(),
-#         "fable_e_other_config": asset_config.FableEOtherConfig(),
-#         "e_model_exclusions": asset_config.EModelExclusions(),
-#         "w_model_exclusions": asset_config.WModelExclusions(),
-#     },
-#     executor=dynamic_executor(
-#         default_config=execution.azure_batch_4cpu_execution_config,
-#         # default_config=execution.basic_execution_config,
-#         # default_config=execution.docker_execution_config,
-#         alternate_configs=[
-#             execution.basic_execution_config,
-#             execution.docker_execution_config,
-#         ],
-#     ),
-# )
